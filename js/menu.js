ccc906cd6a6f72b6ab2b726a6fa23693d2eef370
Matyas Gonzalez <matyas8623@hotmail.com>
2022-08-31 10:34:12 -0300

    archivos web

---
+++ js/menu.js
@@ -0,0 +1,10 @@
+(function(){
+        const openButtom = document.querySelector('nav_menu2');
+
+
+
+        openButtom.addEventListener('click',()=>{
+            alert('ouch , me diste');
+        });
+    
+    })();
